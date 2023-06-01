5a8f8a9514315a94d11d8ee0de33f19ee06acf41
Mikhail <mich_p@inbox.ru>
2023-06-01 22:19:49 +0300

    Добавлены NCSendButton, NCTextField

---
+++ NumiaChat/CustomViews/Buttons/NCSendButton.swift
@@ -0,0 +1,37 @@
+//
+//  NCSendButton.swift
+//  NumiaChat
+//
+//  Created by Миша Перевозчиков on 01.06.2023.
+//
+
+import Foundation
+import UIKit
+
+class NCSendButton: UIButton {
+
+		override init(frame: CGRect) {
+				super.init(frame: frame)
+				configure()
+		}
+
+
+		required init?(coder: NSCoder) {
+				fatalError("init(coder:) has not been implemented")
+		}
+
+
+		private func configure() {
+
+				setImage(UIImage(systemName: "paperplane.fill"), for: .normal)
+				tintColor = .white
+				titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+				translatesAutoresizingMaskIntoConstraints   = false
+		}
+
+
+		func set(backgroundColor: UIColor, title: String)  {
+				self.backgroundColor = backgroundColor
+				setTitle(title, for: .normal)
+		}
+}
---
+++ NumiaChat/CustomViews/TextFields/NCTextField.swift
@@ -0,0 +1,60 @@
+//
+//  NCTextField.swift
+//  NumiaChat
+//
+//  Created by Миша Перевозчиков on 01.06.2023.
+//
+
+import Foundation
+import UIKit
+
+
+final class NCTextField: UITextField {
+
+		override init(frame: CGRect) {
+				super.init(frame: frame)
+				configure()
+		}
+
+
+		required init?(coder: NSCoder) {
+				fatalError("init(coder:) has not been implemented")
+		}
+
+		
+
+
+		private func configure() {
+				translatesAutoresizingMaskIntoConstraints = false
+
+				layer.cornerRadius = 10
+				layer.borderWidth = 2
+				layer.borderColor = UIColor.systemGray4.cgColor
+
+				textColor = .label
+				font = UIFont.preferredFont(forTextStyle: .body)
+				tintColor = .label
+
+				backgroundColor = .tertiarySystemBackground
+				autocorrectionType = .yes
+
+				returnKeyType = .send
+				clearButtonMode = .never
+		}
+
+		
+		let padding = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+
+
+		override public func textRect(forBounds bounds: CGRect) -> CGRect {
+				return bounds.inset(by: padding)
+		}
+
+		override public func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+				return bounds.inset(by: padding)
+		}
+
+		override public func editingRect(forBounds bounds: CGRect) -> CGRect {
+				return bounds.inset(by: padding)
+		}
+}
