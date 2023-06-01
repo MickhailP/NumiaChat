1e5b3d2ec2480cdcc13feb95bcbd4e2cfd2babb7
Mikhail <mich_p@inbox.ru>
2023-06-01 22:20:42 +0300

    Добавлен Chat bar на Main Screen

---
+++ NumiaChat/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  NumiaChat
+//
+//  Created by Миша Перевозчиков on 01.06.2023.
+//
+
+import Foundation
+
+
+struct Message {
+		let time: String
+		let text: String
+}
--- NumiaChat/Screens/MainScreen/MainScreenViewController.swift
+++ NumiaChat/Screens/MainScreen/MainScreenViewController.swift
@@ -7,14 +7,52 @@
 
 import UIKit
 
-class MainScreenViewController: UIViewController {
+class MainScreenViewController: UIViewController, ChatTextfieldDelegate {
+		func didTapSendButton(with message: String) {
+				print("feleate")
+		}
+		
+		let textFieldView = UIView()
+
+		var messages: [Message] = []
+
 
 		override func viewDidLoad() {
 				super.viewDidLoad()
-				// Do any additional setup after loading the view.
-				view.backgroundColor = .red
+
+				configureTextFieldView()
+
+				view.backgroundColor = .blue
 		}
 
+
+		func configureTextFieldView() {
+
+				let textFieldVC = ChatTextfieldVC(delegate: self)
+				add(childVC: textFieldVC, to: textFieldView)
+
+				view.addSubview(textFieldView)
+				textFieldView.translatesAutoresizingMaskIntoConstraints = false
+
+				let padding: CGFloat = 10
+
+				NSLayoutConstraint.activate([
+						textFieldView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: padding),
+						textFieldView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: padding),
+						textFieldView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -padding),
+						textFieldView.heightAnchor.constraint(equalToConstant: 70)
+				])
+
+		}
+
+
+		private func add(childVC: UIViewController, to containerView: UIView) {
+				addChild(childVC)
+				containerView.addSubview(childVC.view)
+				childVC.view.frame = containerView.bounds
+				childVC.didMove(toParent: self)
+				containerView.layer.masksToBounds = true
+		}
 
 }
 
