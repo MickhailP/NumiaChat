0d4fb254cc9c0dfe7afb3f79faf809a79964c4cb
Mikhail <mich_p@inbox.ru>
2023-06-01 14:28:57 +0300

    Initial Commit

---
+++ NumiaChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NumiaChat
+//
+//  Created by Миша Перевозчиков on 01.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
