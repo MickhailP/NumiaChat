6ef424ee94f583df15531b31d0639bdf906a2094
Mikhail <mich_p@inbox.ru>
2023-06-01 15:48:01 +0300

    Настойка проекта, организация папок

---
+++ NumiaChat/AppSceneDelegates/SceneDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  SceneDelegate.swift
+//  NumiaChat
+//
+//  Created by Миша Перевозчиков on 01.06.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+	var window: UIWindow?
+
+
+	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+		guard let windowScene = (scene as? UIWindowScene) else { return }
+
+		window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+		window?.windowScene = windowScene
+		window?.rootViewController = ViewController()
+		window?.makeKeyAndVisible()
+	}
+
+	func sceneDidDisconnect(_ scene: UIScene) {
+
+	}
+
+	func sceneDidBecomeActive(_ scene: UIScene) {
+
+	}
+
+	func sceneWillResignActive(_ scene: UIScene) {
+
+	}
+
+	func sceneWillEnterForeground(_ scene: UIScene) {
+
+	}
+
+	func sceneDidEnterBackground(_ scene: UIScene) {
+
+	}
+}
+
