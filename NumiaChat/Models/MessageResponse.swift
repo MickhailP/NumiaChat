04e49ab63768fb6993c6e4ac8aba60823d8a3954
Mikhail <mich_p@inbox.ru>
2023-06-02 21:46:42 +0300

    Добавлены ModuleBuilder, MessageResponse

---
+++ NumiaChat/Models/MessageResponse.swift
@@ -0,0 +1,13 @@
+//
+//  MessageResponse.swift
+//  NumiaChat
+//
+//  Created by Миша Перевозчиков on 02.06.2023.
+//
+
+import Foundation
+
+
+struct MessageResponse: Decodable {
+		let result: [String]
+}
---
+++ NumiaChat/Screens/ModuleBuilder/ModuleBuilder.swift
@@ -0,0 +1,24 @@
+//
+//  ModuleBuilder.swift
+//  NumiaChat
+//
+//  Created by Миша Перевозчиков on 02.06.2023.
+//
+
+import UIKit
+
+
+protocol Builder {
+		func buildMainScreenModule() -> UIViewController
+}
+
+
+final class ModuleBuilder: Builder {
+
+		func buildMainScreenModule() -> UIViewController {
+				let vc = MainScreenViewController()
+				let presenter = MainScreenPresenter(view: vc, networkingManager: NetworkManager())
+				vc.presenter = presenter
+				return vc
+		}
+}
