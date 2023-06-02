58b2bf519477f5799c18b736f7c4c552ff01af29
Mikhail <mich_p@inbox.ru>
2023-06-02 08:03:23 +0300

    MessageCell  и константы с ID

---
+++ NumiaChat/CustomViews/Cells/MessageCell.swift
@@ -0,0 +1,14 @@
+//
+//  MessageCell.swift
+//  NumiaChat
+//
+//  Created by Миша Перевозчиков on 01.06.2023.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+
+
+}
---
+++ NumiaChat/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  NumiaChat
+//
+//  Created by Миша Перевозчиков on 01.06.2023.
+//
+
+import Foundation
+
+
+struct CellIdentifiers {
+		static let messageCell = "Message Cell"
+}
